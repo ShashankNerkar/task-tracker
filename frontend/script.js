56c1a33581d01421541fffdec1bf714f641d1b85
ShashankNerkar <shashanknerkar21@gmail.com>
2026-09-10 11:35:53 +0530

    Connect frontend to live backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-tracker-ci58.onrender.com/api/tasks";
 
 const taskForm = document.getElementById("taskForm");
 const taskList = document.getElementById("taskList");
