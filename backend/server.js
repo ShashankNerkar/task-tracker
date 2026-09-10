2436650cf2d9b5bcd17ac4160877fca1e439a026
ShashankNerkar <shashanknerkar21@gmail.com>
2026-09-10 11:03:53 +0530

    Use environment port for deployment

--- backend/server.js
+++ backend/server.js
@@ -101,6 +101,8 @@ app.delete("/api/tasks/:id", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
